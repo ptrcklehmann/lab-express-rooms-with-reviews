d2c576c37e418e7f19d26b3b6f25fca706e8414f
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-02-14 21:16:47 +0100

    add user model

--- app.js
+++ app.js
@@ -1,30 +1,98 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require("dotenv/config")
 
 // ℹ️ Connects to the database
-require("./db");
+require("./db")
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require("express")
+
+// session configuration
+// session store using mongo
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const mongoose = require('mongoose')
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require("hbs")
 
-const app = express();
+const app = express()
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
+require("./config")(app)
 
 // default value for title local
-const projectName = "rooms-app";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "express-rooms"
 
-app.locals.title = `${capitalized(projectName)} - ptrcklehmann`;
+app.locals.title = `${projectName} ◦ ptrcklehmann`
 
-// 👇 Start handling routes here
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    saveUninitialized: false,
+    //Forces the session to be saved back to the session store, 
+    // even if the session was never modified during the request.
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
+  })
+)
+// end of session configuration
+
+// passport configuration
+const User = require('./models/User.model')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+// serialize only the `_id` field of the user to keep the information stored minimum
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+})
+
+// when we need the information for the user, the deserializeUser function is called with the id that we previously serialized to fetch the user from the database
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(dbUser => {
+      done(null, dbUser);
+    })
+    .catch(err => {
+      done(err);
+    });
+})
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    // login
+    User.findOne({ username: username })
+      .then(userFromDB => {
+        if (userFromDB === null) {
+          // there is no user with this username
+          done(null, false, { message: 'Wrong Credentials' });
+        } else if (!bcrypt.compareSync(password, userFromDB.password)) {
+          // the password is not matching
+          done(null, false, { message: 'Wrong Credentials' });
+        } else {
+          // the userFromDB should now be logged in
+          done(null, userFromDB)
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 //index route
 const index = require("./routes/index");
 app.use("/", index);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,80 @@
 const router = require("express").Router();
+const User = require('../models/User.model')
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-/* GET home page */
+// check if user is logged in before creating a new account
+const reverseloginCheck = () => {
+  return (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  }
+}
+
+/* GET login page */
 router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
 /* GET signup page */
-router.get("/signup", (req, res, next) => {
+router.get("/signup", reverseloginCheck(), (req, res, next) => {
   res.render("auth/signup");
 });
+
+/* POST login page */
+router.post('/login', 
+    passport.authenticate('local', { 
+    successRedirect: '/',
+    failureRedirect: '/login' 
+  }));
+
+
+
+/* POST signup page */
+router.post('/signup', (req, res, next) => {
+  const { email, password, fullName } = req.body;
+  if (password.length < 8) {
+    res.render('auth/signup', {
+      message: 'Your password must be 8 characters minimun.'
+    });
+    return;
+  }
+  if (email === '') {
+    res.render('auth/signup', { message: 'Your username cannot be empty' });
+    return;
+  }
+  User.findOne({ email: email }).then(found => {
+    if (found !== null) {
+      res.render('auth/signup', { message: 'This username is already taken' });
+    } else {
+      // we can create a user with the username and password pair
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      User.create({ email: email, password: hash, fullName: fullName })
+        .then(dbUser => {
+          // login with passport 
+          req.login(dbUser, err => {
+            if (err) {
+              next(err);
+            } else {
+              res.redirect('/');
+            }
+          })
+        })
+        .catch(err => {
+          next(err);
+        });
+    }
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
 
 module.exports = router;
